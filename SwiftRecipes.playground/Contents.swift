48fcfa1371c1149d6d4d20a3f86484f35b0a18b6
Bhavesh <bhavesh.gupta@techtreeit.com>
2021-08-30 02:08:16 +0530

    completed Beginner Swift Recipes

--- SwiftRecipes.playground/Contents.swift
+++ SwiftRecipes.playground/Contents.swift
@@ -266,3 +266,292 @@ print(oddEven)
 let movieRating: Int? = nil
 
 print(movieRating ?? 1)
+
+/*:
+ ## Problem 3-12
+ **I want to use the logical and operator.**
+
+*/
+
+let isWorkingHard = true
+let isConsistent = true
+
+if isWorkingHard && isConsistent {
+    print("You will get success")
+} else {
+    print("Success is far away")
+}
+
+
+/*:
+ ## Problem 3-13
+ **I want to use the logical not operator.**
+
+*/
+
+var array = [1, 3, 4]
+
+if !array.isEmpty {
+    print("array is not empty it has elements count: \(array.count)")
+}
+
+/*:
+ ## Problem 3-14
+ **I want to use the logical or operator..**
+
+*/
+
+if isWorkingHard || isConsistent {
+    print("Success will come, but it will take time")
+}
+
+
+
+/*:
+ ## Problem 3-15
+ **I want to use the logical xor operator.**
+
+ ### A XOR B => A != B
+
+*/
+
+if isConsistent != isWorkingHard {
+    print("There is something missing")
+} else {
+    print("You may get or not")
+}
+
+/*:
+ ## Problem 4-1
+ **I want to write a for loop..**
+*/
+
+for i in 0...5 {
+    print("index: \(i)")
+}
+
+/*:
+ ## Problem 4-2
+ **I want to write a While loop..**
+*/
+
+var index = 0
+while  index < 5 {
+    index += 1
+    print("Hello World")
+}
+
+
+/*:
+ ## Problem 4-3
+ **I want to write a Repeat-While loop.**
+*/
+
+repeat {
+    print("Hello ")
+    index += 1
+} while index < 10
+
+
+/*:
+ ## Problem 5-1, 5-2
+ **I want to define and call a new function from scratch.**
+
+ **I want to define and call a function that takes arguments.**
+*/
+
+func sayHello(to name: String) {
+    print("Hello \(name), Welcome to Swift Recipe!!!")
+}
+
+sayHello(to: "Bhavesh")
+
+/*:
+ ## Problem 5-3, 5-4
+ **I want to define and call a function that takes arguments with argument labels..**
+
+ **I want to define and call a function without argument labels.**
+*/
+
+func reading(_ book: String, by name: String) {
+    print("\(name) is reading \(book)")
+}
+
+reading("Swift Recipe", by: "Bhavesh")
+
+
+/*:
+ ## Problem 5-5
+ **I want to define and call a function, using default values.**
+*/
+
+func greet(_ name: String, with word: String = "Hey") {
+    print("\(word) \(name)")
+}
+
+greet("Bhavesh", with: "Hello")
+greet("Bhavesh", with: "Bonjour")
+greet("Bhavesh")
+
+
+/*:
+ ## Problem 5-6
+ **I want to define and call a function that returns some value.**
+*/
+
+func getIntroduction(_ name: String, with age: Int, profession: String) -> String {
+    return "My Name is \(name), I am \(age) years old. I am \(profession)"
+}
+
+let introduction = getIntroduction("Bhavesh", with: 25, profession: "iOS Developer")
+print(introduction)
+
+/*:
+ ## Problem 5-7
+ **I want to define and call a function that returns multiple values.**
+*/
+
+@discardableResult
+func divide(_ a: Int, with b: Int) -> (Int, Int) {
+    let quotient = a / b
+    let remainder = a % b
+    return (quotient: quotient, remainder: remainder)
+}
+
+let result = divide(37, with: 5)
+print(result)
+
+/*:
+ ## Problem 5-8
+ **I want to call a function but ignore its return value.**
+*/
+
+// 1- way using @discardable Result
+divide(48, with: 7)
+
+//2- way using underscore wild card
+let _ = divide(90, with: 14)
+
+/*:
+ ## Problem 5-9
+ **I want to define and call a function with in-out arguments, so that I can pass arguments “by reference.”**
+
+ #### We’ll do that as usual, only we’ll put an & in front of the variable, in order to pass it “by reference” – so that the function can actually alter it.
+*/
+
+class Person {
+    var name: String
+    var age: Int
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
+extension Person: CustomStringConvertible {
+    var description: String {
+        return "\(name) : \(age)"
+    }
+}
+
+var bhavesh = Person(name: "Bhavesh", age: 25)
+print(bhavesh)
+
+func incrementAge(_ person: inout Person) {
+    person.age += 1
+}
+incrementAge(&bhavesh)
+print(bhavesh)
+
+
+/*:
+ ## Problem 5-10
+ **I want to define and call a function with variadic arguments, that is: with an unspecified number of arguments.**
+*/
+
+func sum(_ values: Int...) -> Int {
+    return values.reduce(0) { $0 + $1 }
+}
+
+let sumOfValues = sum(1, 4, 5, 2, 23, 90, 123)
+print(sumOfValues)
+
+/*:
+ ## Problem 5-11
+ **I want to define and call a function that takes a closure as argument.**
+
+ ## IMPORTANT: Asks into many interview questions
+*/
+
+func task(completion: (_ taskName: String) -> Void) {
+    completion("Reading")
+}
+
+task { taskName in
+    print("\(taskName) the task here")
+}
+
+func perform(_ a: Int, _ b: Int, action: (Int, Int) -> Int) -> Int {
+    return action(a,b)
+}
+
+let sumAction = { (a: Int, b: Int) -> Int in
+    return a + b
+}
+
+let multiplyAction = { (a: Int, b: Int) -> Int in
+    return a * b
+}
+
+let sumNumber = perform(6, 7, action: sumAction)
+print(sumNumber)
+
+let multiplyNumber = perform(4, 89, action: multiplyAction)
+print(multiplyNumber)
+
+
+/*:
+ ## Problem 5-12
+ **I want to call a function with a trailing closure argument.**
+
+ ## IMPORTANT: Asks into many interview questions
+*/
+
+let subtractNumber = perform(20, 8) { (a, b) -> Int in
+    return a - b
+}
+print(subtractNumber)
+
+let subtractNumberComplex = perform(20, 7) { $0 - $1 }
+print(subtractNumberComplex)
+
+
+/*:
+ ## Problem 5-13, 5-14
+ **I want to define and call a function that takes arguments of a generic type, that is: without a specific already-known type.**
+
+ **I want to define and call a function that takes arguments of a generic type but with constraints, that is: without a specific already-known type but conforming to some specific protocol.**
+
+ ## IMPORTANT: Asks into many interview questions
+*/
+
+//func sumGeneric<T: Decimal>(_ a: T, _ b: T) -> T {
+//    return a + b
+//}
+
+//sumGeneric(60, 80)
+
+/*:
+ ## Problem 5-15
+ **I want to define and call nested functions.**
+*/
+
+func greeting(_ name: String, dobYear: Int) -> String{
+    func getAge(from dobYear: Int) -> Int {
+        return 2021 - dobYear
+    }
+
+    return "\(name): \(getAge(from: dobYear))"
+}
+
+print(greeting("Bhavesh", dobYear: 1990))
